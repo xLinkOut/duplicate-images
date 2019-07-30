26634d27b4669c86f80508abee52d9edddcc7c00
Luca <luca.cirillo5@gmail.com>
2019-07-30 18:15:53 +0200

    Explore the given path for files and subdirectories, then create a generator to use into add function to hash files.

--- src/Main.py
+++ src/Main.py
@@ -3,6 +3,7 @@
 
 from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
+import magic
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Database.db'
@@ -32,6 +33,26 @@ def index():
 @app.route('/add', methods=['POST'])
 def add():
     path = request.form['path']
+    imagesList = exploreDir(path)
     return render_template("add.html", path=path)
+
+def exploreDir(path):
+    for root, directories, filenames in os.walk(path, topdown=True):
+        # Process file into root
+        for filename in filenames:
+            filepath = os.path.join(root,filename)
+            if isImage(filepath):
+                yield filepath
+        # Then process subdirectories
+        for subdir in directories:
+            exploreDir(subdir)
+
+def isImage(filepath):
+    full_supported_formats = ['gif', 'jp2', 'jpeg', 'pcx', 'png', 'tiff', 'x-ms-bmp', 'x-portable-pixmap', 'x-xbitmap']
+    try:
+        mime = magic.from_file(filepath, mime=True)
+        return mime.rsplit('/', 1)[1] in full_supported_formats
+    except IndexError:
+        return False
 
 app.run(debug = True)
