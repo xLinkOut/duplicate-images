f527551be447014f54ca605ab8c4444e2f83d22f
Luca <luca.cirillo5@gmail.com>
2019-07-30 18:12:58 +0200

    Base route for index.html; model for 'Files' table into DB.

--- src/Main.py
+++ src/Main.py
@@ -10,4 +10,25 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+# Table "Files" in SQLite DB
+class Files(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    path = db.Column(db.String(512),unique = True, nullable = False)
+    hashes = db.Column(db.String(512), nullable = False)
+    file_size = db.Column(db.Integer, nullable = True)
+    image_size = db.Column(db.String(128), nullable = True)
+    capture_time = db.Column(db.String(256), nullable = True)
+    
+    def __repr__(self):
+        return "<Files %s>" % self.path
+
+# Create all tables into DB
+#db.create_all()
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
 app.run(debug = True)
